5c8ac5a43d3f363505f37f8f5b9046400ae399c6
MTRNord <mtrnord1@gmail.com>
2018-02-19 14:48:17 +0100

    Fix use of papers-list

--- src/app/papers/papers.component.ts
+++ src/app/papers/papers.component.ts
@@ -9,20 +9,9 @@ import {isPlatformBrowser} from '@angular/common';
 export class PapersComponent {
   pageTitle = 'Find Papers';
   pageSubtitle = 'Get the full blast of different Newspapers';
-  testData: Array<object> = [{title: 'Freifunk News', uuid: 0, logoURL: 'https://blog.freifunk.net/wp-content/uploads/2017/03/Freifunk.net_.svg_.png', description: ''}];
-  public chunkedData = this.chunk(this.testData, 3);
   isBrowser: boolean;
 
   constructor( @Inject(PLATFORM_ID) platformId: Object) {
     this.isBrowser = isPlatformBrowser(platformId);
   }
-
-  chunk(arr, size) {
-    const newArr = [];
-    for (let i = 0; i < arr.length; i += size) {
-      newArr.push(arr.slice(i, i + size));
-    }
-    return newArr;
-  }
-
 }
