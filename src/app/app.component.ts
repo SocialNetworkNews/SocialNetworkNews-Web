44af380b1b2af00a1a12bb18d4bc05854bbf7428
MTRNord <mtrnord1@gmail.com>
2018-03-21 20:14:55 +0100

    Remove totally unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +6,4 @@ import {NavigationEnd, Router} from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  currentUrl: string;
-  constructor(router: Router) {
-    router.events.subscribe((value) => {
-      if (value instanceof NavigationEnd ) {
-        this.currentUrl = value.url;
-      }
-    });
-  }
-
-  checkCurrent(path = '/'): string {
-    const active =  (this.currentUrl === path) ? 'active' : '';
-    console.log('Route: ' + path + ' Current: ' + this.currentUrl +  ' Active?: ' + active);
-    return active;
-  }
-
 }
