5265fe84cc6d31568a4664ce96cdf22c95978969
MTRNord <mtrnord1@gmail.com>
2018-03-23 15:43:39 +0100

    Remove extends

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import {AbstractLoader} from 'ng-http-loader/components/abstract.loader.component';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent extends AbstractLoader {
+export class SpinnerComponent {
 }
