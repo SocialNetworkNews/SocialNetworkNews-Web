acea83492a5254977a84d9b6a8ccc6f5bb2fe6ee
MTRNord <mtrnord1@gmail.com>
2018-05-19 15:47:15 +0200

    Update karma for travis tests

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,19 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     browsers: ['Chrome'],
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: [
+          // '--no-sandbox',
+          // See https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+        ],
+      }
+    },
     singleRun: true
   });
 };
